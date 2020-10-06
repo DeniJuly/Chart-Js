3e2079960328046bdaf2e7da040eb153eed2e7c8
DeniJuliSetiawan112 <denijuli.setiawan@gmail.com>
2020-10-06 22:22:22 +0700

    update

---
+++ js/dataChart.js
@@ -0,0 +1,366 @@
+window.randomInt = function (min = 20, max = 100) {
+	return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+let dataChart = {
+	januari: {
+		label: ["Label 1", "Label 2", "Label 3", "Label 4", "Label 5"],
+		dataset: [
+			{
+				data: [randomInt(), randomInt(), randomInt(), randomInt(), randomInt()],
+				backgroundColor: "#008FFB",
+				label: "Data 1",
+			},
+			{
+				data: [randomInt(), randomInt(), randomInt(), randomInt(), randomInt()],
+				backgroundColor: "#00E396",
+				label: "Data 2",
+			},
+			{
+				data: [randomInt(), randomInt(), randomInt(), randomInt(), randomInt()],
+				backgroundColor: "#FEB019",
+				label: "Data 3",
+			},
+			{
+				data: [randomInt(), randomInt(), randomInt(), randomInt(), randomInt()],
+				backgroundColor: "#FF4560",
+				label: "Data 4",
+			},
+			{
+				data: [randomInt(), randomInt(), randomInt(), randomInt(), randomInt()],
+				backgroundColor: "#775DD0",
+				label: "Data 5",
+			},
+		],
+	},
+	februari: {
+		label: ["Label 1", "Label 2", "Label 3", "Label 4", "Label 5"],
+		dataset: [
+			{
+				data: [randomInt(), randomInt(), randomInt(), randomInt(), randomInt()],
+				backgroundColor: "#008FFB",
+				label: "Data 1",
+			},
+			{
+				data: [randomInt(), randomInt(), randomInt(), randomInt(), randomInt()],
+				backgroundColor: "#00E396",
+				label: "Data 2",
+			},
+			{
+				data: [randomInt(), randomInt(), randomInt(), randomInt(), randomInt()],
+				backgroundColor: "#FEB019",
+				label: "Data 3",
+			},
+			{
+				data: [randomInt(), randomInt(), randomInt(), randomInt(), randomInt()],
+				backgroundColor: "#FF4560",
+				label: "Data 4",
+			},
+			{
+				data: [randomInt(), randomInt(), randomInt(), randomInt(), randomInt()],
+				backgroundColor: "#775DD0",
+				label: "Data 5",
+			},
+		],
+	},
+	maret: {
+		label: ["Label 1", "Label 2", "Label 3", "Label 4", "Label 5"],
+		dataset: [
+			{
+				data: [randomInt(), randomInt(), randomInt(), randomInt(), randomInt()],
+				backgroundColor: "#008FFB",
+				label: "Data 1",
+			},
+			{
+				data: [randomInt(), randomInt(), randomInt(), randomInt(), randomInt()],
+				backgroundColor: "#00E396",
+				label: "Data 2",
+			},
+			{
+				data: [randomInt(), randomInt(), randomInt(), randomInt(), randomInt()],
+				backgroundColor: "#FEB019",
+				label: "Data 3",
+			},
+			{
+				data: [randomInt(), randomInt(), randomInt(), randomInt(), randomInt()],
+				backgroundColor: "#FF4560",
+				label: "Data 4",
+			},
+			{
+				data: [randomInt(), randomInt(), randomInt(), randomInt(), randomInt()],
+				backgroundColor: "#775DD0",
+				label: "Data 5",
+			},
+		],
+	},
+	april: {
+		label: ["Label 1", "Label 2", "Label 3", "Label 4", "Label 5"],
+		dataset: [
+			{
+				data: [randomInt(), randomInt(), randomInt(), randomInt(), randomInt()],
+				backgroundColor: "#008FFB",
+				label: "Data 1",
+			},
+			{
+				data: [randomInt(), randomInt(), randomInt(), randomInt(), randomInt()],
+				backgroundColor: "#00E396",
+				label: "Data 2",
+			},
+			{
+				data: [randomInt(), randomInt(), randomInt(), randomInt(), randomInt()],
+				backgroundColor: "#FEB019",
+				label: "Data 3",
+			},
+			{
+				data: [randomInt(), randomInt(), randomInt(), randomInt(), randomInt()],
+				backgroundColor: "#FF4560",
+				label: "Data 4",
+			},
+			{
+				data: [randomInt(), randomInt(), randomInt(), randomInt(), randomInt()],
+				backgroundColor: "#775DD0",
+				label: "Data 5",
+			},
+		],
+	},
+	mei: {
+		label: ["Label 1", "Label 2", "Label 3", "Label 4", "Label 5"],
+		dataset: [
+			{
+				data: [randomInt(), randomInt(), randomInt(), randomInt(), randomInt()],
+				backgroundColor: "#008FFB",
+				label: "Data 1",
+			},
+			{
+				data: [randomInt(), randomInt(), randomInt(), randomInt(), randomInt()],
+				backgroundColor: "#00E396",
+				label: "Data 2",
+			},
+			{
+				data: [randomInt(), randomInt(), randomInt(), randomInt(), randomInt()],
+				backgroundColor: "#FEB019",
+				label: "Data 3",
+			},
+			{
+				data: [randomInt(), randomInt(), randomInt(), randomInt(), randomInt()],
+				backgroundColor: "#FF4560",
+				label: "Data 4",
+			},
+			{
+				data: [randomInt(), randomInt(), randomInt(), randomInt(), randomInt()],
+				backgroundColor: "#775DD0",
+				label: "Data 5",
+			},
+		],
+	},
+	juni: {
+		label: ["Label 1", "Label 2", "Label 3", "Label 4", "Label 5"],
+		dataset: [
+			{
+				data: [randomInt(), randomInt(), randomInt(), randomInt(), randomInt()],
+				backgroundColor: "#008FFB",
+				label: "Data 1",
+			},
+			{
+				data: [randomInt(), randomInt(), randomInt(), randomInt(), randomInt()],
+				backgroundColor: "#00E396",
+				label: "Data 2",
+			},
+			{
+				data: [randomInt(), randomInt(), randomInt(), randomInt(), randomInt()],
+				backgroundColor: "#FEB019",
+				label: "Data 3",
+			},
+			{
+				data: [randomInt(), randomInt(), randomInt(), randomInt(), randomInt()],
+				backgroundColor: "#FF4560",
+				label: "Data 4",
+			},
+			{
+				data: [randomInt(), randomInt(), randomInt(), randomInt(), randomInt()],
+				backgroundColor: "#775DD0",
+				label: "Data 5",
+			},
+		],
+	},
+	juli: {
+		label: ["Label 1", "Label 2", "Label 3", "Label 4", "Label 5"],
+		dataset: [
+			{
+				data: [randomInt(), randomInt(), randomInt(), randomInt(), randomInt()],
+				backgroundColor: "#008FFB",
+				label: "Data 1",
+			},
+			{
+				data: [randomInt(), randomInt(), randomInt(), randomInt(), randomInt()],
+				backgroundColor: "#00E396",
+				label: "Data 2",
+			},
+			{
+				data: [randomInt(), randomInt(), randomInt(), randomInt(), randomInt()],
+				backgroundColor: "#FEB019",
+				label: "Data 3",
+			},
+			{
+				data: [randomInt(), randomInt(), randomInt(), randomInt(), randomInt()],
+				backgroundColor: "#FF4560",
+				label: "Data 4",
+			},
+			{
+				data: [randomInt(), randomInt(), randomInt(), randomInt(), randomInt()],
+				backgroundColor: "#775DD0",
+				label: "Data 5",
+			},
+		],
+	},
+	agustus: {
+		label: ["Label 1", "Label 2", "Label 3", "Label 4", "Label 5"],
+		dataset: [
+			{
+				data: [randomInt(), randomInt(), randomInt(), randomInt(), randomInt()],
+				backgroundColor: "#008FFB",
+				label: "Data 1",
+			},
+			{
+				data: [randomInt(), randomInt(), randomInt(), randomInt(), randomInt()],
+				backgroundColor: "#00E396",
+				label: "Data 2",
+			},
+			{
+				data: [randomInt(), randomInt(), randomInt(), randomInt(), randomInt()],
+				backgroundColor: "#FEB019",
+				label: "Data 3",
+			},
+			{
+				data: [randomInt(), randomInt(), randomInt(), randomInt(), randomInt()],
+				backgroundColor: "#FF4560",
+				label: "Data 4",
+			},
+			{
+				data: [randomInt(), randomInt(), randomInt(), randomInt(), randomInt()],
+				backgroundColor: "#775DD0",
+				label: "Data 5",
+			},
+		],
+	},
+	september: {
+		label: ["Label 1", "Label 2", "Label 3", "Label 4", "Label 5"],
+		dataset: [
+			{
+				data: [randomInt(), randomInt(), randomInt(), randomInt(), randomInt()],
+				backgroundColor: "#008FFB",
+				label: "Data 1",
+			},
+			{
+				data: [randomInt(), randomInt(), randomInt(), randomInt(), randomInt()],
+				backgroundColor: "#00E396",
+				label: "Data 2",
+			},
+			{
+				data: [randomInt(), randomInt(), randomInt(), randomInt(), randomInt()],
+				backgroundColor: "#FEB019",
+				label: "Data 3",
+			},
+			{
+				data: [randomInt(), randomInt(), randomInt(), randomInt(), randomInt()],
+				backgroundColor: "#FF4560",
+				label: "Data 4",
+			},
+			{
+				data: [randomInt(), randomInt(), randomInt(), randomInt(), randomInt()],
+				backgroundColor: "#775DD0",
+				label: "Data 5",
+			},
+		],
+	},
+	oktober: {
+		label: ["Label 1", "Label 2", "Label 3", "Label 4", "Label 5"],
+		dataset: [
+			{
+				data: [randomInt(), randomInt(), randomInt(), randomInt(), randomInt()],
+				backgroundColor: "#008FFB",
+				label: "Data 1",
+			},
+			{
+				data: [randomInt(), randomInt(), randomInt(), randomInt(), randomInt()],
+				backgroundColor: "#00E396",
+				label: "Data 2",
+			},
+			{
+				data: [randomInt(), randomInt(), randomInt(), randomInt(), randomInt()],
+				backgroundColor: "#FEB019",
+				label: "Data 3",
+			},
+			{
+				data: [randomInt(), randomInt(), randomInt(), randomInt(), randomInt()],
+				backgroundColor: "#FF4560",
+				label: "Data 4",
+			},
+			{
+				data: [randomInt(), randomInt(), randomInt(), randomInt(), randomInt()],
+				backgroundColor: "#775DD0",
+				label: "Data 5",
+			},
+		],
+	},
+	november: {
+		label: ["Label 1", "Label 2", "Label 3", "Label 4", "Label 5"],
+		dataset: [
+			{
+				data: [randomInt(), randomInt(), randomInt(), randomInt(), randomInt()],
+				backgroundColor: "#008FFB",
+				label: "Data 1",
+			},
+			{
+				data: [randomInt(), randomInt(), randomInt(), randomInt(), randomInt()],
+				backgroundColor: "#00E396",
+				label: "Data 2",
+			},
+			{
+				data: [randomInt(), randomInt(), randomInt(), randomInt(), randomInt()],
+				backgroundColor: "#FEB019",
+				label: "Data 3",
+			},
+			{
+				data: [randomInt(), randomInt(), randomInt(), randomInt(), randomInt()],
+				backgroundColor: "#FF4560",
+				label: "Data 4",
+			},
+			{
+				data: [randomInt(), randomInt(), randomInt(), randomInt(), randomInt()],
+				backgroundColor: "#775DD0",
+				label: "Data 5",
+			},
+		],
+	},
+	desember: {
+		label: ["Label 1", "Label 2", "Label 3", "Label 4", "Label 5"],
+		dataset: [
+			{
+				data: [randomInt(), randomInt(), randomInt(), randomInt(), randomInt()],
+				backgroundColor: "#008FFB",
+				label: "Data 1",
+			},
+			{
+				data: [randomInt(), randomInt(), randomInt(), randomInt(), randomInt()],
+				backgroundColor: "#00E396",
+				label: "Data 2",
+			},
+			{
+				data: [randomInt(), randomInt(), randomInt(), randomInt(), randomInt()],
+				backgroundColor: "#FEB019",
+				label: "Data 3",
+			},
+			{
+				data: [randomInt(), randomInt(), randomInt(), randomInt(), randomInt()],
+				backgroundColor: "#FF4560",
+				label: "Data 4",
+			},
+			{
+				data: [randomInt(), randomInt(), randomInt(), randomInt(), randomInt()],
+				backgroundColor: "#775DD0",
+				label: "Data 5",
+			},
+		],
+	},
+};
